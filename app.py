335f8f5f3708aab39e3e0d0fd00e3d1e35bbcf05
edimeza71-collab <edimeza71@gmail.com>
2026-07-13 19:06:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ clave_secreta = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=clave_secreta)
 
 # --- CONFIGURACIÓN DE CONEXIONES ---
-FIREBASE_URL = "https://monitoreoia-b2097-default-rtdb.firebaseio.com/.json"
+FIREBASE_URL = "https://monitoreoia-b2097-default-rtdb.firebaseio.com/datos.json"
 https:"//monitoreoia-b2097-default-rtdb.firebaseio.com"
 BOT_TOKEN = "8916674528:AAG0uHgWcg-5h4QB_BqidoNUQPyxBHZ3Ebc"
 CHAT_ID = "1476571501"
